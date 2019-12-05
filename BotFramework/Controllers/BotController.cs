7e4f07405ec0f39b1f5b56e4ac5f185f88162d66
Maxi Gómez <56489609+maxiggomez@users.noreply.github.com>
2019-12-05 18:31:44 -0300

    Agrgar elemento a COle mediante POST

--- BotFramework/Controllers/BotController.cs
+++ BotFramework/Controllers/BotController.cs
@@ -5,6 +5,7 @@
 
 using System;
 using System.IO;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Bot.Builder;
@@ -12,6 +13,7 @@ using Microsoft.Bot.Builder.Integration.AspNet.Core;
 using Microsoft.Bot.Schema;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using Microsoft.Extensions.Configuration;
 
 namespace BotFramework.Controllers
 {
@@ -22,14 +24,25 @@ namespace BotFramework.Controllers
     [ApiController]
     public class BotController : ControllerBase
     {
-        private readonly IBotFrameworkHttpAdapter Adapter;
+        private readonly IBotFrameworkHttpAdapter _adapter;
         private readonly IBot Bot;
         private readonly Conversations _conversations;
-        public BotController(IBotFrameworkHttpAdapter adapter, IBot bot, Conversations conversations)
+        private readonly string _appId;
+        public BotController(IBotFrameworkHttpAdapter adapter, IConfiguration configuration, IBot bot, Conversations conversations)
         {
-            Adapter = adapter;
+            _adapter = adapter;
             Bot = bot;
             _conversations = conversations ?? throw new ArgumentNullException(nameof(conversations));
+
+            _appId = configuration["MicrosoftAppId"];
+
+            // If the channel is the Emulator, and authentication is not in use,
+            // the AppId will be null.  We generate a random AppId for this case only.
+            // This is not required for production, since the AppId will have a value.
+            if (string.IsNullOrEmpty(_appId))
+            {
+                _appId = Guid.NewGuid().ToString(); //if no AppId, use a random Guid
+            }
         }
 
         [HttpPost, HttpGet]
@@ -37,7 +50,7 @@ namespace BotFramework.Controllers
         {
             // Delegate the processing of the HTTP POST to the adapter.
             // The adapter will invoke the bot.
-            await Adapter.ProcessAsync(Request, Response, Bot);
+            await _adapter.ProcessAsync(Request, Response, Bot);
         }
 
 
@@ -46,7 +59,7 @@ namespace BotFramework.Controllers
         {
             string body = await new StreamReader(Request.Body).ReadToEndAsync();
 
-            var valores = JsonConvert.DeserializeObject<OrdenMessage>(body);
+            var valores = JsonConvert.DeserializeObject<OrderMessage>(body);
 
             var conversation = _conversations.Get(valores.MesssageId);
 
@@ -55,28 +68,36 @@ namespace BotFramework.Controllers
                 return new InvokeResponse { Status = 404, Body = body };
             }
 
-            await ((BotAdapter)Adapter).ContinueConversationAsync("botId", conversation, async (context, token) =>
-            {
-                context.Activity.Name = "orderRPAResult";
-                context.Activity.Text = String.Format("Resultado recibido para MensajeId:{0}\r\nOrden:{1}\r\nMonto:{2}\r\nEstado:{3}",valores.MesssageId,valores.OrdenId,valores.Total,valores.Estado);
-                context.Activity.Type = "event";
-                context.Activity.RelatesTo = null;
-                //context.Activity.Value = body;
-
-                await Bot.OnTurnAsync(context, token);
-            },default);
-
-            await Adapter.ProcessAsync(Request, Response, Bot);
+            await SendMessageToBot(valores, conversation);
 
             return new InvokeResponse { Status = 200, Body = body };
         }
 
+        private async Task SendMessageToBot(OrderMessage valores, ConversationReference conversation)
+        {
+            await ((BotAdapter)_adapter).ContinueConversationAsync(_appId, conversation, async (context, token) =>
+            {
+                context.Activity.Name = "orderRPAResult";
+                context.Activity.Text = String.Format("Resultado recibido para MensajeId:{0}\r\nOrden:{1}\r\nMonto:{2}\r\nEstado:{3}", valores.MesssageId, valores.OrderId, valores.Total, valores.Estado);
+                context.Activity.Type = "event";
+                //context.Activity.RelatesTo = null;
+                //context.Activity.Value = body;
+
+                await Bot.OnTurnAsync(context, token);
+            }, default);
+
+            //await _adapter.ProcessAsync(Request, Response, Bot);
+        }
     }
 
-    public class OrdenMessage
+
+    public class OrderMessage
     {
-        public string OrdenId { get; set; }
         public string MesssageId { get; set; }
+        public string OrderId { get; set; }
+        public string company { get; set; }
+        public string date { get; set; }
+        public string applicant { get; set; }
         public string Total { get; set; }
         public string Estado { get; set; }
     }
